2b7aa278690a493f5a1c21c25780ba973ad0d2b1
LancemDev <munyaolance1@gmail.com>
2024-11-14 07:18:56 +0300

    Feat: Added assets

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
+
+# Configuration for static files directory
+STATIC_FOLDER = os.path.join(os.getcwd(), 'api/static')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(STATIC_FOLDER, filename)
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'This is the static subdomain!'
 
 @app.route('/about')
 def about():
     return 'About'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
